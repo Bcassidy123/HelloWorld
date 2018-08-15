ac7499216394cfaafb30f477d56f833e5a3a19ae
Bcassidy123 <40898648+Bcassidy123@users.noreply.github.com>
2018-08-16 05:39:18 +1000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,5 +10,6 @@ int main()
   std::cout << "Dumpster!" << std::endl;
   std::cout << "Hello, Cassidy!" << std::endl;
   std::cout << "Recompile pleasse" << std::endl;
+  std::cout << "Does a thing pleasse" << std::endl;
   return 0;
 }
