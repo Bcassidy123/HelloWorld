561291730ac1fbc27377dc177fcb198b41532db1
Bcassidy123 <40898648+Bcassidy123@users.noreply.github.com>
2018-08-16 04:26:18 +1000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main()
 {
   std::cout << "ANd dagain" << std::endl;
   std::cout << "Dumpster!" << std::endl;
-  std::cout << "Hello, World!" << std::endl;
+  std::cout << "Hello, Cassidy!" << std::endl;
   std::cout << "Recompile pleasse" << std::endl;
   return 0;
 }
